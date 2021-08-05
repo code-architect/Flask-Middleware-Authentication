4bda903094551801b751fd14d87858dc1f8e9940
Indranil Samanta <indranil.samanta@varadhismartek.in>
2021-08-05 18:41:20 +0530

    created the basic shell, added middlewares, created the enviorment for database connection(added in .env_example file)

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import logging
 from flask import Flask
 
-# middleware here
+from library.flask.account import FlaskAccount
+from middleware import AuthTokenCheck, DBConnect
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-#app.wsgi_app = middleware(app.wsgi_app)
+app.wsgi_app = AuthTokenCheck(app.wsgi_app)
+app.wsgi_app = DBConnect(app.wsgi_app)
 
 
-app.add_url_rule('/account', view_func=, methods=['PUT'])
-app.add_url_rule('/account', view_func=, methods=['POST'])
-app.add_url_rule('/account/sign-in', view_func=, methods=['GET'])
-app.add_url_rule('/account/sign-out', view_func=, methods=['GET'])
+app.add_url_rule('/account', view_func=FlaskAccount.create, methods=['PUT'])
+app.add_url_rule('/account', view_func=FlaskAccount.update, methods=['POST'])
+app.add_url_rule('/account/sign-in', view_func=FlaskAccount.sign_in, methods=['GET'])
+app.add_url_rule('/account/sign-out', view_func=FlaskAccount.sign_out, methods=['GET'])
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', '5000', debug=True)
+    app.run('127.0.0.1', '5000', debug=True)
---
+++ middleware/__init__.py
@@ -0,0 +1,41 @@
+import os
+import logging
+from werkzeug.wrappers import Request, Response
+from mongoengine import connect
+
+
+class AuthTokenCheck:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+
+        logging.info('AuthToken check - ok')
+        return self.app(environ, start_response)
+
+
+class DBConnect:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        try:
+            connect(
+                db=os.getenv('MONGO_DATABASE'),
+                host=os.getenv('MONGO_HOST'),
+                port=int(os.getenv('MONGO_PORT')) if os.getenv('MONGO_PORT') else None,
+                username=os.getenv('MONGO_USERNAME'),
+                password=os.getenv('MONGO_PASSWORD'),
+                authentication_source=os.getenv('MONGO_AUTH_SOURCE'),
+                authentication_mechanism=os.getenv('MONGO_MECHANISM')
+            )
+            logging.info('connection to mongo success')
+        except Exception as e:
+            logging.info('connection to mongo failed')
+            logging.info('connection error', e)
+
+            res = Response(u'Connection to mongo failed', mimetype='text/plain', status=501)
+            return res(environ, start_response)
+
+
