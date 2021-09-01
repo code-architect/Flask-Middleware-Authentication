f86c1f8db1a1533326ca2ddfb9ec2f1cb9454035
Indranil Samanta <indranil.samanta@varadhismartek.in>
2021-09-01 22:12:05 +0530

    implimented sign_in method, creating token based on password and email, created enviorment variable secret

--- library/flask/account.py
+++ library/flask/account.py
@@ -11,6 +11,7 @@ from odm.account import Account
 
 
 class FlaskAccount:
+
     @staticmethod
     def create():
         json_data = request.get_json()
@@ -37,7 +38,7 @@ class FlaskAccount:
 
         return Response(dumps({
             'status': 'ok',
-            "code": 200
+            'code': 200
         }), mimetype='text/json'), 200
 
     @staticmethod
@@ -46,7 +47,47 @@ class FlaskAccount:
 
     @staticmethod
     def sign_in():
-        pass
+        logging.info('execute account sign in')
+
+        email = request.args.get('email')
+        password = request.args.get('password')
+
+        account = Account.objects(email=email).only(
+            'password',
+            'token'
+        ).first()
+
+        if account:
+            if pbkdf2_sha512.verify(password, account['password']):
+                logging.info('sign in success')
+
+                token_encode = jwt.encode({
+                    'id': str(account.pk),
+                    'token': account.token
+                }, os.getenv('JWT_TOKEN_SECRET'), algorithm='HS256')
+
+                return Response(dumps({
+                    'status': 'ok',
+                    'token': token_encode,
+                    # 'token': jwt.decode(token_encode, os.getenv('JWT_TOKEN_SECRET'), algorithms=["HS256"]),
+                    'code': 200
+                }), mimetype='text/json'), 200
+            else:
+                logging.info('sign in unauthorized: wrong password')
+
+                return Response(dumps({
+                    'status': 'unauthorized',
+                    'data': 'Unauthorized User',
+                    'code': 401
+                }), mimetype='text/json'), 401
+        else:
+            logging.info('sign in unauthorized: account not exists')
+            
+            return Response(dumps({
+                'status': 'unauthorized',
+                'data': 'Unauthorized User',
+                'code': 401
+            }), mimetype='text/json'), 401
 
     @staticmethod
     def sign_out():
